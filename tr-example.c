271d6f7f0a469c7705e45c40dca51fe307ce092c
napro1299 <blaketadner@gmail.com>
2023-12-26 00:20:40 -0500

    shader pipeline and mesh drawing in progress

--- tr-example.c
+++ tr-example.c
@@ -9,15 +9,34 @@
 #define WIDTH 75
 #define HEIGHT 16*3
 
+static void vertex_shader_func(vertex_t vertex, v2f *position, void *out_data) {
+    // pixel type
+    *(char *)out_data = '+';
+    *position = (v2f) {vertex.vertex.x, vertex.vertex.y};
+}
+
+static void fragment_shader_func(void *in_data, char *pixel_type) {
+    *pixel_type = *(char *)in_data;
+}
+
 int main() {
     tr_t tr = tr_create(WIDTH, HEIGHT);
     tr_clear(&tr);
+
+    vertex_t triangle[] = {
+        {{-0.5f, -0.5f, 0.0f}, {0}},
+        {{0.5f, -0.5f, 0.0f}, {0}},
+        {{0.0f, 0.5f, 0.0f}, {0}}
+    };
+
+    shader_t basic_shader = {vertex_shader_func, fragment_shader_func};
+    tr_set_shader(basic_shader);
 
     float timer = 0.0;
     while (1) {
         timer += 0.001;
         tr_draw_line(&tr, 0, 0, fabsf(cosf(timer/10)) * WIDTH, fabsf(sinf(timer/14)) * HEIGHT);
-        tr_draw_text(&tr, 10, 16*2, "hello", 5);
+        // tr_draw_text(&tr, 10, 16*2, "hello", 5);
         tr_render(&tr);
         sleep(0.2);
 
--- tr.h
+++ tr.h
@@ -14,7 +14,7 @@
 #define xytoi(x, y, width) (((y) * (width)) + (x))
 #define ASSERT(_e, ...) if (!(_e)) { fprintf(stderr, __VA_ARGS__); exit(1); }
 
-#define ASSERT_BOUNDS(x, y, width, height) ASSERT(x <= width - 1 && y <= height - 1, "Draw out of bounds")
+#define ASSERT_BOUNDS(x, y, width, height) ASSERT(x <= width && y <= height, "Draw out of bounds (%i, %i)\n", x, y)
 
 typedef struct {
     size_t width;
@@ -24,11 +24,44 @@ typedef struct {
     char *printbuffer;
 } tr_t;
 
+typedef struct {
+    float x, y;
+} v2f;
+
+typedef struct {
+    float x, y, z;
+} v3f;
+
+typedef struct {
+    v3f vertex;
+    v3f normal;
+} vertex_t;
+
+/**
+ * Vertex shader program:
+ *  - in: vertex
+ *  - out:
+ *      - position in clip space
+ *      - data
+ * 
+ * Fragment shader program:
+ *  - in: data
+ *  - out: pixel type
+*/
+typedef struct {
+    void (*vertex_program)(vertex_t vertex, v2f *position, void *out_data);
+    void (*fragment_program)(void *in_data, char *pixel_type);
+} shader_t;
+
+static shader_t _shader = {0, 0};
+
 tr_t tr_create(size_t width, size_t height);
 void tr_render(tr_t *tr);
 void tr_clear(tr_t *tr);
-void tr_draw_line(tr_t *tr, size_t x0, size_t y0, size_t x1, size_t y1);
 void tr_draw_text(tr_t *tr, size_t x, size_t y, char *text, size_t len);
+void tr_draw_line(tr_t *tr, size_t x0, size_t y0, size_t x1, size_t y1);
+void tr_draw_mesh(tr_t *tr, vertex_t *vertices, size_t vertices_size);
+void tr_set_shader(shader_t shader);
 
 static void setc(tr_t *tr, size_t x, size_t y, char c);
 
@@ -69,6 +102,9 @@ void tr_render(tr_t *tr) {
 }
 
 void tr_draw_line(tr_t *tr, size_t x0, size_t y0, size_t x1, size_t y1) {
+    ASSERT_BOUNDS(x0, y0, tr->width, tr->height);
+    ASSERT_BOUNDS(x1, y1, tr->width, tr->height);
+
     bool steep = false;
     int tmp;
     if (abs(x0-x1)<abs(y0-y1)) { 
@@ -97,7 +133,7 @@ void tr_draw_line(tr_t *tr, size_t x0, size_t y0, size_t x1, size_t y1) {
         if (steep) { 
             setc(tr, y, x, '0');
         } else { 
-            setc(tr, x, y, '0'); 
+            setc(tr, x, y, '0');
         } 
         error += derror; 
         if (error>0.5) { 
@@ -111,6 +147,10 @@ void tr_draw_text(tr_t *tr, size_t x, size_t y, char *text, size_t len) {
     ASSERT_BOUNDS(x, y, tr->width, tr->height);
     size_t idx = xytoi(x, y, tr->width);
     memcpy(&tr->framebuffer[idx], text, len > (tr->width * tr->height) - idx ? (tr->width * tr->height) - idx : len);
+}
+
+void tr_set_shader(shader_t shader) {
+    _shader = shader;
 }
 
 #endif // TR_IMPLEMENTATION
